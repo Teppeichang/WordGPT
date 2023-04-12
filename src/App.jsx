8cb8a29b6d896b7679c35b321f736b895e75d653
Teppeichang <teppeichan9@gmail.com>
2023-04-12 17:38:25 +0900

    一旦コミット(OpenAI APIへのリクエストまで実装)

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,56 @@
 import axios from "axios";
-import { TextField } from "@mui/material";
-import { Button } from "@mui/material";
+import { useState } from "react";
+import { TextField, Button } from "@mui/material";
 
 const App = () => {
-  const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+  const [message, setMessage] = useState("");
+  const [response, setResponse] = useState("");
+
+  const sendMessage = async(event) => {
+    event.preventDefault();
+    const URL = "https://api.openai.com/v1/chat/completions";
+    try{
+      const response = await axios.post(
+        URL,
+        {
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              role: "user",
+              content: message,
+            },
+          ],
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+          },
+        }
+      );
+      console.log(response)
+      setResponse(response);
+    } catch(error) {
+      console.log(error)
+    }
+  };
+
   return (
     <div className="min-h-screen flex flex-col items-center">
-      <TextField label="回答" multiline rows={4} />
-      <form className="flex flex-row items-center">
-        <TextField label="何か入力してください" variant="outlined" />
-        <Button variant="contained">Submit</Button>
+      <p className="mt-10">WordGPT</p>
+      <form className="flex flex-col items-center my-5" onSubmit={sendMessage}>
+        <TextField
+          label="何か入力してください"
+          variant="outlined"
+          className="w-80"
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+        />
+        <Button variant="contained" sx={{ my: 2 }} type="submit">
+          文章作成
+        </Button>
       </form>
+      <TextField label="回答" multiline rows={4} className="w-80" value={response} />
     </div>
   );
 };
