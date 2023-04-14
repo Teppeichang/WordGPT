4b797e45d7f5bdfbea3ce094c666db28cffefb77
Teppeichang <teppeichan9@gmail.com>
2023-04-14 17:58:58 +0900

    一旦コミット(WordPress REST APIへのリクエスト実装)

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,23 @@ import { useState } from "react";
 import { TextField } from "@mui/material";
 
 const App = () => {
-  const [message, setMessage] = useState("");
+  const OPENAI_API_REQUEST_URL = "https://api.openai.com/v1/chat/completions";
+  const WP_REST_API_REQUEST_URL = "http://localhost:8080/wp-json/wp/v2/posts";
+
+  const [prompt, setPrompt] = useState("");
   const [response, setResponse] = useState("");
 
-  const sendMessage = async (event) => {
+  const sendPrompt = async (event) => {
     event.preventDefault();
-    const URL = "https://api.openai.com/v1/chat/completions";
     try {
       const response = await axios.post(
-        URL,
+        OPENAI_API_REQUEST_URL,
         {
           model: "gpt-3.5-turbo",
           messages: [
             {
               role: "user",
-              content: message,
+              content: prompt,
             },
           ],
         },
@@ -34,23 +36,56 @@ const App = () => {
     }
   };
 
+  const postArticle = async (event) => {
+    event.preventDefault();
+    try {
+      await axios
+        .post(
+          WP_REST_API_REQUEST_URL,
+          {
+            title: "WP REST APIからの投稿",
+            content: response,
+            status: "draft",
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Basic ${btoa(`wp_test_user:${process.env.REACT_APP_WP_APP_PASSWORD}`)}`
+            },
+          }
+        )
+        .then((res) => {
+          if (res.status === 200) {
+            console.log("投稿に成功しました");
+          }
+        });
+    } catch (error) {
+      console.log(`${error}: 投稿に失敗しました`);
+    }
+  };
+
   return (
     <div className="min-h-screen flex flex-col mx-20 ">
       <p className="my-10 text-center text-3xl font-bold">WordGPT</p>
-      <div className="flex flex-col bg-slate-200 rounded-lg p-10">
-        <form className="flex flex-col justify-center my-5" onSubmit={sendMessage}>
+      <div className="flex flex-col bg-slate-100 rounded-lg p-10">
+        <form className="flex flex-col justify-center my-5" onSubmit={sendPrompt}>
           <TextField
             label="何か入力してください"
             variant="outlined"
             className="bg-white"
-            value={message}
-            onChange={(event) => setMessage(event.target.value)}
+            value={prompt}
+            onChange={(event) => setPrompt(event.target.value)}
           />
           <button type="submit" className="bg-blue-400 text-white py-2 mt-2 mb-10 rounded-lg w-80">
             文章作成
           </button>
         </form>
-        <TextField label="回答" multiline rows={10} value={response} className="bg-white" />
+        <form className="flex flex-col justify-center" onSubmit={postArticle}>
+          <TextField label="回答" multiline rows={10} value={response} className="bg-white" />
+          <button type="submit" className="bg-blue-400 text-white py-2 mt-2 mb-10 rounded-lg w-80">
+            WP投稿
+          </button>
+        </form>
       </div>
     </div>
   );
